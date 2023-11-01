9b3e2861a96775af581efd75469c40285a27602a
nanikon <milkchocolatewithdaisy@gmail.com>
2023-11-01 16:34:53 +0300

    add sender

--- lab1/Core/Src/main.c
+++ lab1/Core/Src/main.c
@@ -123,6 +123,7 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+	  printf("%i %i", is_pressed, is_wait_unpressed);
 	  if (0 == is_pressed && 0 == is_wait_unpressed) {
 		  if (count_tick > LONG_PERIOD_CT && 0 != pointer){
 			  is_reading = 0;
